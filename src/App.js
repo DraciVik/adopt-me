7994a1c6dfc650eed38e9c88bf4d021aa2ba24a2
DraciVik <viktor.stojanov90@gmail.com>
2019-09-19 08:16:38 +0200

    Added jsx and configured ESLint to work with the project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Marko", animal: "cat", breed: "Stupid" }),
-    React.createElement(Pet, { name: "Zutko", animal: "cat", breed: "Stray" }),
-    React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Stupid" })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, { name: "Marko", animal: "cat", breed: "Stupid" }),
+  //   React.createElement(Pet, { name: "Zutko", animal: "cat", breed: "Stray" }),
+  //   React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Stupid" })
+  // ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt me!</h1>
+      <Pet name="Zutko" animal="Cat" breed="Best ever" />
+      <Pet name="Marko" animal="Cat" breed="Demon" />
+      <Pet name="Donka" animal="Dog" breed="Mudd" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
