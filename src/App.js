95fbf70f39799223b4f7fd623b38ace4cc256d95
DraciVik <viktor.stojanov90@gmail.com>
2019-09-20 09:44:17 +0200

    Added useState for setting location and a custom hook for animal type and breed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, { name: "Marko", animal: "cat", breed: "Stupid" }),
-  //   React.createElement(Pet, { name: "Zutko", animal: "cat", breed: "Stray" }),
-  //   React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Stupid" })
-  // ]);
   return (
     <div>
       <h1 id="something-important">Adopt me!</h1>
-      <Pet name="Zutko" animal="Cat" breed="Best ever" />
-      <Pet name="Marko" animal="Cat" breed="Demon" />
-      <Pet name="Donka" animal="Dog" breed="Mudd" />
+      <SearchParams />
     </div>
   );
 };
